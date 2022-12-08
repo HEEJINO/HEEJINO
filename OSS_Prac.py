48ed87e1344fc3ab2a78bfd10582473f381e9569
Heejin Moom <shahee200234@naver.com>
2022-12-08 20:50:31 +0900

    Second Commit

--- OSS_Prac.py
+++ OSS_Prac.py
@@ -4,3 +4,20 @@
 # 1&2 성공 시 얼굴에서 하얀색이 차지하는 비율 검출 -도이
 # 그 비율에 따라서 마스크 0,X유무 판단
 
+# 사진 파일 하나 더 추가함 img1으로
+
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default_.xml')
+
+img = cv2.imread('./image/heejin.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    print(x, y, w, h)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    
+cv2.namedWindow('Heejin', cv2.WINDOW_NORMAL) //사용자가 사진의 크기를 바꿀 수 있음
+cv2.imshow('Heejin', img)
+cv2.waitKey()
